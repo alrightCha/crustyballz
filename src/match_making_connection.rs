39fbffd6442ffaa55389bbea6a09392cf83b7c94
alrightCha <ychamam7@gmail.com>
2024-08-26 23:56:40 +0100

    fix

--- src/match_making_connection.rs
+++ src/match_making_connection.rs
@@ -5,7 +5,7 @@ use log::info;
 use tokio::net::TcpStream;
 use tokio::sync::Mutex;
 use tokio_tungstenite::tungstenite::protocol::Message;
-use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream, connect_async_tls_with_config};
+use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 
 pub struct MatchMakingConnection {
     socket_sender: Mutex<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>>,
@@ -17,8 +17,7 @@ impl MatchMakingConnection {
     }
 
     pub async fn setup_connection(url: String) -> MatchMakingConnection {
-        let tls_connector = TlsConnector::from(native_tls::TlsConnector::new().unwrap());
-        let (ws_stream, _) = connect_async_tls_with_connector(url, Some(tls_connector))
+        let (ws_stream, _) = connect_async(url)
             .await
             .expect("Failed to connect matchmaking service");
         let (write, read) = ws_stream.split();
