553dd40543def8cbc83cd6a8b3ea8e890c063f64
alrightCha <ychamam7@gmail.com>
2024-09-15 19:37:14 +0200

    fix

--- src/utils/util.rs
+++ src/utils/util.rs
@@ -163,18 +163,18 @@ pub fn find_index(arr: &[Player], id: PlayerID) -> Option<usize> {
     )
 }
 
-//cheks which cell ate the other one by knowing which one is bigger, if there is an overlap between the cells
+//checks which cell ate the other one by knowing which one is bigger, if there is an overlap between the cells
 pub fn check_who_ate_who(cell_a: &Cell, cell_b: &Cell) -> u8 {
     if check_overlap(&cell_a.position, &cell_b.position) {
         let min_cell_rad = f32::min(cell_a.position.radius, cell_b.position.radius);
         if min_cell_rad == cell_a.position.radius {
             // Since we're working with integers, compare cell_a.mass to 5/6 of cell_b.mass instead of multiplying
-            if cell_b.mass > (cell_a.mass * 5) / 6 {
+            if cell_b.mass > (cell_a.mass * 9) / 6 {
                 return 2;
             }
         } else {
             // Similar comparison but for cell_b against cell_a
-            if cell_a.mass > (cell_b.mass * 5) / 6 {
+            if cell_a.mass > (cell_b.mass * 9) / 6 {
                 return 1;
             }
         }
