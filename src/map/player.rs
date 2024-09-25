db2b94ef376245988685686a2d526eb7baca2ce1
alrightCha <ychamam7@gmail.com>
2024-09-25 23:42:22 +0200

    fix ?

--- src/map/player.rs
+++ src/map/player.rs
@@ -544,7 +544,6 @@ impl Player {
                 self.target_y,
                 slow_base,
                 init_mass_log,
-                self.ratio,
             );
             adjust_for_boundaries(
                 &mut cell.position.x,
