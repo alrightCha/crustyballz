ea7be0e1f340187034977750313f46501aca2123
alrightCha <ychamam7@gmail.com>
2024-09-30 23:06:40 +0200

    check

--- src/map/cell.rs
+++ src/map/cell.rs
@@ -94,7 +94,7 @@ impl Cell {
     ) {
         let target_x = player_position.x - self.position.x + mouse_x;
         let target_y = player_position.y - self.position.y + mouse_y;
-        let dist = (target_y.powi(4) + target_x.powi(4)).sqrt();
+        let dist = (target_y.powi(2) + target_x.powi(2)).sqrt();
         let mut deg = target_y.atan2(target_x);
         if deg.sin() < 0.04 {
             deg += 0.04; // Small adjustment to avoid exact zero
@@ -114,8 +114,8 @@ impl Cell {
             delta_y = self.speed * deg.sin() / slow_down;
             delta_x = self.speed * deg.cos() / slow_down;
 
-            if dist * rat < self.position.radius {
-                let ratio = dist * rat / (self.position.radius);
+            if dist < self.position.radius {
+                let ratio = dist / (self.position.radius);
                 delta_y *= ratio;
                 delta_x *= ratio;
             }
