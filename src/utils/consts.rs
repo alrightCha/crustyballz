f4606967523335874f917f74a3f486f0d76e9f44
alrightCha <ychamam7@gmail.com>
2024-09-30 19:52:49 +0200

    new number

--- src/utils/consts.rs
+++ src/utils/consts.rs
@@ -3,6 +3,6 @@ pub type Mass = u16;
 
 pub const MIN_DISTANCE: f32 = 40.0;
 pub const MIN_SPEED: f32 = 9.0;
-pub const PUSHING_AWAY_SPEED: f32 = 10.0;
+pub const PUSHING_AWAY_SPEED: f32 = 5.0;
 pub const MERGE_TIMER: f32 = 20.0;
 pub const SPLIT_CELL_SPEED: f32 = 70.0;
