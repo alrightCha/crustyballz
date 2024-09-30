e16de0130eaf980322dff844f0ebb6647f84a71b
alrightCha <ychamam7@gmail.com>
2024-09-30 20:10:28 +0200

    better ?

--- src/map/player.rs
+++ src/map/player.rs
@@ -498,13 +498,11 @@ impl Player {
                 x: cell_b.position.x - cell_a.position.x,
                 y: cell_b.position.y - cell_a.position.y,
                 radius: 0.0,
-            }
-            .normalize()
-            .scale(PUSHING_AWAY_SPEED);
-            cell_a.position.x -= vector.x;
-            cell_a.position.y -= vector.y;
-            cell_b.position.x += vector.x;
-            cell_b.position.y += vector.y;
+            };
+            cell_a.position.x -= vector.x / 2.0;
+            cell_a.position.y -= vector.y / 2.0;
+            cell_b.position.x += vector.x / 2.0;
+            cell_b.position.y += vector.y / 2.0;
         });
     }
 
