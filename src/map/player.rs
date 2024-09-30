d51984f47a42786ae504e21372f633c158167b4a
alrightCha <ychamam7@gmail.com>
2024-09-30 22:33:21 +0200

    trying new scale

--- src/map/player.rs
+++ src/map/player.rs
@@ -504,7 +504,7 @@ impl Player {
                 radius: 0.0,
             }
             .normalize()
-            .scale(10.0 / (cell_b.position.distance(&cell_a.position)));
+            .scale(100.0 / (cell_b.position.distance(&cell_a.position)));
 
             cell_a.position.x -= vector.x;
             cell_a.position.y -= vector.y;
