809f53fec34ea56258cc578354b04b783083f2b8
Lucas Campos <lcmc@cin.ufpe.br>
2024-10-30 17:33:02 -0300

    added method to send datagrams

--- src/player_connection.rs
+++ src/player_connection.rs
@@ -1,5 +1,6 @@
+use log::info;
 use tokio::sync::Mutex;
-use wtransport::Connection;
+use wtransport::{error::SendDatagramError, Connection};
 
 use crate::{recv_messages::AnyEventPacket, send_messages::SendEvent};
 
@@ -19,12 +20,13 @@ impl PlayerConnection {
         }
     }
 
-    pub async fn emit_bi_buffer(&self, buffer: &Vec<u8>) {
+    pub async fn emit_bi_buffer(&self, buffer: &Vec<u8>) { // -> Result<usize, StreamWriteError>
         let mut send_stream = self.send_bi_stream.lock().await;
         let _ = send_stream.write(&buffer).await;
     }
 
-    pub async fn emit_bi<T: serde::Serialize>(&self, send_event: SendEvent, data: T) {
+    pub async fn emit_bi<T: serde::Serialize>(&self, send_event: SendEvent, data: T) { // -> Result<usize, StreamWriteError>
+        info!("Sending event[{}] to player - emit_bi", send_event);
         let packet = AnyEventPacket::new(
             send_event,
             data,
@@ -33,6 +35,19 @@ impl PlayerConnection {
         let buffer = packet.to_buffer();
         self.emit_bi_buffer(&buffer).await;
     }
+    
+    pub fn emit_datagram<T: serde::Serialize>(&self, send_event: SendEvent, data: T) -> Result<(), SendDatagramError> {
+        info!("Sending event[{}] to player - datagram", send_event);
+        let packet = AnyEventPacket::new(
+            send_event,
+            data,
+        );
 
-    pub async fn emit_unidirection<T: serde::Serialize>(&self, send_event: SendEvent, data: T) {}
+        let buffer = packet.to_buffer();
+        self.emit_datagram_buffer(&buffer)
+    }
+
+    pub fn emit_datagram_buffer(&self, buffer: &Vec<u8>) -> Result<(), SendDatagramError>{
+        self.w_connection.send_datagram(buffer)
+    }
 }
