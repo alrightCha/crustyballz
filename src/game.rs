04fbbb3e13fe099c31e3edc06a118bea1e2d2673
alrightCha <ychamam7@gmail.com>
2024-12-07 04:48:12 +0100

    fix

--- src/game.rs
+++ src/game.rs
@@ -197,7 +197,7 @@ impl Game {
                     .await;
 
                  //Remove player from game
-                 self.remove_player(&mut_player.id);
+                 self.remove_player(&mut_player.id).await;
                 //Send Kick player from game
                 match self.get_player_stream(mut_player.id).await {
                     Some(cash_out_player_connection) => {
